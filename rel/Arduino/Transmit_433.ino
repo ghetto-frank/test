17d7284a7db37b08fa9e412ca561f7c513da6320
ghetto-frank <32285759+ghetto-frank@users.noreply.github.com>
2017-09-26 05:21:05 +0100

    Update Transmit_433.ino

--- rel/Arduino/Transmit_433.ino
+++ rel/Arduino/Transmit_433.ino
@@ -10,10 +10,18 @@
 // =!=done=!=
 // (not including \n newlines)
 // -------
+// clear plans to incorprate IR Tranmiter support as mays well as not had to implemnt in the python script.
+//IR LIB USED URL: https://github.com/z3t0/Arduino-IRremote NOTE: The IR LED Uses PWM pin 3.
 
 
-#include <RCSwitch.h>
+#include <RCSwitch.h> // for 433mhz transmiter on pin 10.
+#include <IRremote.h> // for ir led on pin 3.
 
+
+// -----------------------
+
+
+IRsend irsend;
 RCSwitch mySwitch = RCSwitch();
 
 // ------------------------------------------------
@@ -47,26 +55,28 @@ void loop() { //---START MAIN LOOP
         readString.toLowerCase();  //nice eh, easyer for if statement. 
         // -----------------------------------------------------------------------------      
 
-
+        // ================OTHER COMMANDS=============
+        
         // -----------------is_ready command--------------------
         if (readString.substring(0,8) == "is_ready"){
             Serial.println("");
-            Serial.println("=!v!=READY");
+            Serial.println("=!v!=READY"); //let software knows where ready and communicating ok.
             Serial.println("");    
             Serial.println("=!=done=!="); //tell our python script where done sending.
             Serial.println("");      
         }
         // -------------------------------------------------
 
-
-        // -----------------send command--------------------
-        if (readString.substring(0,5) == "send:"){
+        // ================RADIO COMMANDS=============
+        
+        // -----------------radio send command--------------------
+        if (readString.substring(0,11) == "radio_send:"){
     
             String code_str;
             unsigned long int code_int;
                      
-            code_str = readString.substring(5,readString.length()); //remove first 5 chars.
-            code_str.replace(" ", "");
+            code_str = readString.substring(11,readString.length()); //remove "radio_send:".
+            code_str.replace(" ", ""); 
             code_int = code_str.toInt(); //convert to an int value to be accepted by mySwitch.
             
             mySwitch.send(code_int, 24); // send 24 bit code in decimal.
@@ -78,6 +88,36 @@ void loop() { //---START MAIN LOOP
             Serial.println("");       
         }
         // -------------------------------------------------
+        
+        
+        // ================IR COMMANDS================
+        
+        
+        // ----------------TEST-----------------------------
+        if (readString.substring(0,11) == "ir_send:red"){
+            irsend.sendNEC(0xF720DF, 32); //*4. 32 send the code useing NEC protocal.
+            Serial.println("=!v!=OK");
+            Serial.println("=!=done=!=");
+        }
+        // -------------------------------------------------
+
+        // ----------------TEST-----------------------------
+        if (readString.substring(0,13) == "ir_send:green"){
+            irsend.sendNEC(0xF7A05F, 32); //*4.
+            Serial.println("=!v!=OK");
+            Serial.println("=!=done=!=");
+        }
+        // -------------------------------------------------
+
+        // ----------------TEST-----------------------------
+        if (readString.substring(0,12) == "ir_send:blue"){
+            irsend.sendNEC(0xF7609F, 32); //*4. 32 
+            Serial.println("=!v!=OK");
+            Serial.println("=!=done=!=");
+        }
+        // -------------------------------------------------
+        
+        
         
         
     } // ---END OF SERIAL COMMANDS---
