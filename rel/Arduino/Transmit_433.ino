39ef161991b118e5ce99bf716da564574b92bbc9
ghetto-frank <32285759+ghetto-frank@users.noreply.github.com>
2017-09-26 04:50:03 +0100

    Update and rename Transmit_33.ino to Transmit_433.ino

--- rel/Arduino/Transmit_433.ino
+++ rel/Arduino/Transmit_433.ino
@@ -1,9 +1,20 @@
 
+// Transmit_433 Program.
+// -------
+// simple program that send sends 24 bit, 433mhz decimal codes that is passed by
+// a 115200 baud rate serial connection. example serial session:
+// #SEND:
+// send:0000000
+// #RECV:
+// =!v!=Sent: 0000000
+// =!=done=!=
+// (not including \n newlines)
+// -------
+
 
 #include <RCSwitch.h>
 
 RCSwitch mySwitch = RCSwitch();
-
 
 // ------------------------------------------------
 void setup() {
@@ -11,7 +22,6 @@ void setup() {
     mySwitch.enableTransmit(10);  // Using Pin #10
 }
 // ------------------------------------------------
-
 
 
 // --------------------------------------------------------------------------------------------------------      
@@ -22,10 +32,10 @@ void loop() { //---START MAIN LOOP
     if (Serial.available()) { // ---START OF SERIAL COMMANDS---
    
         // -----------------------Build seralport data into readString------------------        
-        String readString = "";
+        String readString = ""; //clear string.
     
         while (Serial.available()) {
-            delay(2);
+            delay(2); //account for any lag. 
         
             if (Serial.available() > 0) {
                 char c = Serial.read();  //gets one byte from serial buffer        
@@ -34,7 +44,7 @@ void loop() { //---START MAIN LOOP
          
         }
     
-        readString.toLowerCase();  //nice eh
+        readString.toLowerCase();  //nice eh, easyer for if statement. 
         // -----------------------------------------------------------------------------      
 
 
@@ -43,7 +53,7 @@ void loop() { //---START MAIN LOOP
             Serial.println("");
             Serial.println("=!v!=READY");
             Serial.println("");    
-            Serial.println("=!=done=!=");  
+            Serial.println("=!=done=!="); //tell our python script where done sending.
             Serial.println("");      
         }
         // -------------------------------------------------
@@ -55,21 +65,21 @@ void loop() { //---START MAIN LOOP
             String code_str;
             unsigned long int code_int;
                      
-            code_str = readString.substring(5,readString.length());
+            code_str = readString.substring(5,readString.length()); //remove first 5 chars.
             code_str.replace(" ", "");
-            code_int = code_str.toInt();
+            code_int = code_str.toInt(); //convert to an int value to be accepted by mySwitch.
             
-            mySwitch.send(code_int, 24); // send code.
+            mySwitch.send(code_int, 24); // send 24 bit code in decimal.
             Serial.println("");
             Serial.print("=!v!=Sent: ");
             Serial.println("");
             Serial.println(code_int);     
-            Serial.println("=!=done=!=");
+            Serial.println("=!=done=!="); //tell python script done.
             Serial.println("");       
         }
         // -------------------------------------------------
-
-
+        
+        
     } // ---END OF SERIAL COMMANDS---
   
 delay(1); //stop maxing out CPU.
